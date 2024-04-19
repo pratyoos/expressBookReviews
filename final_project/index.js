ab7bd883ad69014016691518cc8a0911175ff764
Pratyoos Panta <pratyoos.panta@outlook.com>
2024-04-19 11:27:51 +0545

    Update index.js

--- final_project/index.js
+++ final_project/index.js
@@ -4,34 +4,6 @@ const session = require('express-session')
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
 
-
-let users = []
-//Function to check if the user exists
-const doesExist = (username)=>{
-  let userswithsamename = users.filter((user)=>{
-    return user.username === username
-  });
-  if(userswithsamename.length > 0){
-    return true;
-  } else {
-    return false;
-  }
-}
-//Function to check if the user is authenticated
-const authenticatedUser = (username,password)=>{
-  let validusers = users.filter((user)=>{
-    return (user.username === username && user.password === password)
-  });
-  if(validusers.length > 0){
-    return true;
-  } else {
-    return false;
-  }
-}
-
-
-
-
 const app = express();
 
 app.use(express.json());
@@ -40,9 +12,9 @@ app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUni
 
 app.use("/customer/auth/*", function auth(req,res,next){
 //Write the authenication mechanism here
-if(req.session.authorization) { //get the authorization object stored in the session
-    token = req.session.authorization['accessToken']; //retrieve the token from authorization object
-    jwt.verify(token, "access",(err,user)=>{ //Use JWT to verify token
+if(req.session.authorization) {
+    let token = req.session.authorization['accessToken']; // Access Token
+    jwt.verify(token, "access",(err,user)=>{
         if(!err){
             req.user = user;
             next();
@@ -55,13 +27,9 @@ if(req.session.authorization) { //get the authorization object stored in the ses
      return res.status(403).json({message: "User not logged in"})
  }
 });
-
-
-
  
 const PORT =5000;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
-
 app.listen(PORT,()=>console.log("Server is running"));
